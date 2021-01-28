e61622a4c58b6c73207dc87c1754fc995a0d6494
virtualabs <virtualabs@gmail.com>
2021-01-29 00:56:06 +0100

    Improved listbox widget (scroll, selection, animation).

--- inc/ui/listbox.h
+++ inc/ui/listbox.h
@@ -1,9 +1,16 @@
 #ifndef __INC_WIDGET_LISTBOX_H
 #define __INC_WIDGET_LISTBOX_H
 
+#include "ui/widget.h"
 #include "ui/container.h"
+#include "ui/scrollbar.h"
 
 #define LISTBOX_STYLE_BORDER RGB(0xf, 0xf, 0xf)
+
+typedef enum {
+  LB_STATE_IDLE,
+  LB_STATE_MOVING
+} widget_listbox_anim_state_t;
 
 typedef struct {
 
@@ -13,16 +20,28 @@ typedef struct {
   /* Container. */
   widget_container_t container;
 
+  /* Scrollbar. */
+  widget_scrollbar_t scrollbar;
+
   /* Parameters. */
   int n_items;
-  int selected_index;
+  widget_t *p_selected_item;
 
-  /* TODO: scrollbar. */
+  /* Scrolling. */
+  widget_listbox_anim_state_t state;
+  int move_orig_x;
+  int move_orig_y;
+  int move_orig_offset;
+  volatile int offset;
+
+  /* Animation */
+  float speed;
 
 } widget_listbox_t;
 
 void widget_listbox_init(widget_listbox_t *p_widget_listbox, tile_t *p_tile, int x, int y, int width, int height);
 void widget_listbox_add(widget_listbox_t *p_widget_listbox, widget_t *p_widget);
 void widget_listbox_remove(widget_listbox_t *p_widget_listbox, widget_t *p_widget);
+int widget_listbox_count(widget_listbox_t *p_widget_listbox);
 
 #endif /* __INC_WIDGET_LISTBOX_H */
--- ui/listbox.c
+++ ui/listbox.c
@@ -1,4 +1,57 @@
 #include "ui/listbox.h"
+#include "esp_log.h"
+
+#define TAG "listbox"
+
+void widget_listbox_animate(widget_listbox_t *p_listbox)
+{
+  /* Determine a dy based on current scrolling speed. */
+  if (p_listbox->speed < 0)
+  {
+    if (p_listbox->offset < 0)
+    {
+      p_listbox->offset -= (int)(p_listbox->speed/4);
+      if (p_listbox->offset >= 0)
+      {
+        /* Animation done, switch to idle. */
+        p_listbox->offset = 0;
+        p_listbox->state = LB_STATE_IDLE;
+      }
+    }
+
+    /* Adapt speed. */
+    p_listbox->speed = (9.9*p_listbox->speed)/10.0;
+  }
+  else if (p_listbox->speed > 0)
+  {
+    if (p_listbox->offset > -(p_listbox->scrollbar.max - 20))
+    {
+      p_listbox->offset -= (int)(p_listbox->speed/4);
+      if (p_listbox->offset <= -(p_listbox->scrollbar.max - 20))
+      {
+        /* Animation done, switch to idle. */
+        p_listbox->offset = -(p_listbox->scrollbar.max - 20);
+        p_listbox->state = LB_STATE_IDLE;
+      }
+    }
+
+    /* Adapt speed. */
+    p_listbox->speed = (9*p_listbox->speed)/10.0;
+  }
+  else
+  {
+    /* Animation done, switch to idle. */
+    p_listbox->offset = -(p_listbox->scrollbar.max - 20);
+    p_listbox->state = LB_STATE_IDLE;
+  }
+}
+
+
+/**
+ * widget_listbox_drawfunc()
+ * 
+ * @brief: Specific rendering function for our listbox widget.
+ **/
 
 void widget_listbox_drawfunc(widget_t *p_widget)
 {
@@ -11,8 +64,28 @@ void widget_listbox_drawfunc(widget_t *p_widget)
     p_listbox->container.widget.box.x = widget_get_abs_x(p_widget) + 1;
     p_listbox->container.widget.box.y = widget_get_abs_y(p_widget) + 1;
 
+    /* Animate container if required. */
+    if (p_listbox->state == LB_STATE_IDLE)
+    {
+      p_listbox->container.offset_y = p_listbox->offset;
+      p_listbox->scrollbar.value = -p_listbox->offset;
+    }
+    else
+    {
+      widget_listbox_animate(p_listbox);
+      p_listbox->container.offset_y = p_listbox->offset;
+      p_listbox->scrollbar.value = -p_listbox->offset;
+    }
+
+    /* Position scrollbar (position not updated while animating). */
+    p_listbox->scrollbar.widget.box.x = widget_get_abs_x(p_widget) + p_listbox->widget.box.width - 10;
+    p_listbox->scrollbar.widget.box.y = widget_get_abs_y(p_widget);
+
     /* Draw container. */
     widget_draw(&p_listbox->container);
+
+    /* Draw scrollbar. */
+    widget_draw(&p_listbox->scrollbar);
 
     /* Draw listbox borders. */
    /* Draw the button. */
@@ -51,6 +124,152 @@ void widget_listbox_drawfunc(widget_t *p_widget)
   }
 }
 
+
+int widget_listbox_event_handler(widget_t *p_widget, widget_event_t event, int x, int  y, int velocity)
+{
+  bool b_processed = false;
+  widget_container_item_t *p_item = NULL;
+  widget_listbox_t *p_listbox = (widget_listbox_t *)p_widget->p_user_data;
+
+  if (p_listbox != NULL)
+  {
+    switch(event)
+    {
+
+      case WE_TAP:
+        {
+          /* Check if coordinates match a widget. */
+          p_item = p_listbox->container.p_children;
+          if (p_item != NULL)
+          {
+            do
+            {
+              /* Check if tap happened in this widget. */
+              if (
+                (p_item->p_widget->box.x <= x) &&
+                ((p_item->p_widget->box.x + p_item->p_widget->box.width) > x) &&
+                (p_item->p_widget->box.y <= y) &&
+                ((p_item->p_widget->box.y + p_item->p_widget->box.height) > y)
+              )
+              {
+                /* Deselect previous item if any. */
+                if (p_listbox->p_selected_item != NULL)
+                  widget_send_event(p_listbox->p_selected_item, LB_ITEM_DESELECTED, 0, 0, 0);
+
+                /* Select this item. */
+                widget_send_event(p_item->p_widget, LB_ITEM_SELECTED, 0, 0, 0);
+                p_listbox->p_selected_item = p_item->p_widget;
+              }
+              p_item = p_item->p_next;
+            }
+            while (p_item != NULL);
+          }
+        }
+        break;
+
+      case WE_PRESS:
+        {
+          if (p_listbox->state == LB_STATE_MOVING)
+          {
+            p_listbox->state = LB_STATE_IDLE;
+            b_processed = true;
+          }
+        }
+        break;
+
+      case WE_SWIPE_UP:
+        {
+          ESP_LOGI(TAG, "[listbox] event: swipe up (%d)", velocity);
+          if (p_listbox->offset == -(p_listbox->scrollbar.max - 20))
+          {
+            /* No animation. */
+            p_listbox->state = LB_STATE_IDLE;
+          }
+          else
+          {
+            /* Animate. */
+            p_listbox->state = LB_STATE_MOVING;
+            p_listbox->speed = velocity;
+
+          }
+          b_processed = true;
+        }
+        break;
+
+      case WE_SWIPE_DOWN:
+        {
+          ESP_LOGI(TAG, "[listbox] event: swipe down (%d)", velocity);
+          if (p_listbox->offset == 0)
+          {
+            /* No animation. */
+            p_listbox->state = LB_STATE_IDLE;
+          }
+          else
+          {
+            /* Animate. */
+            p_listbox->state = LB_STATE_MOVING;
+            p_listbox->speed = -velocity;
+
+          }
+          b_processed = true;
+        }
+        break;
+
+      default:
+      {
+        p_listbox->state = LB_STATE_IDLE;
+        b_processed = true;
+      }
+      break;
+    }
+
+    /* Notify UI if event has been processed or not. */
+    return b_processed;
+  }
+
+  /* Event not processed. */
+  return WE_ERROR;
+}
+
+
+/* Update scrollbar. */
+void update_scrollbar(widget_listbox_t *p_widget_listbox)
+{
+  widget_container_item_t *p_item = NULL;
+  int height = 0;
+
+  /* Compute total height and update scrollbar values. */
+  if (p_widget_listbox->container.p_children != NULL)
+  {
+    p_item = p_widget_listbox->container.p_children;
+    height = p_item->rel_box.y;
+    while (p_item->p_next != NULL)
+    {
+      height += p_item->rel_box.height;
+      p_item = p_item->p_next;
+    }
+
+    if (p_item != NULL)
+    {
+      height += p_item->rel_box.height;
+    }
+  }
+
+  p_widget_listbox->scrollbar.max = height;
+}
+
+/**
+ * widget_listbox_init()
+ * 
+ * @brief: Initialize a listbox widget
+ * @param p_widget_listbox: Pointer to a `widget_listbox_t` structure
+ * @param p_tile: Pointer to a `tile_t` structure
+ * @param x: X position of the widget
+ * @param y: Y position of the widget
+ * @param width: widget width
+ * @param height: widget height
+ **/
+
 void widget_listbox_init(widget_listbox_t *p_widget_listbox, tile_t *p_tile, int x, int y, int width, int height)
 {
   /* Initialize our widget. */
@@ -62,20 +281,48 @@ void widget_listbox_init(widget_listbox_t *p_widget_listbox, tile_t *p_tile, int
     NULL,
     x + 1,
     y + 1,
-    width - 2,
+    width - 12,
     height - 2
+  );
+
+  widget_scrollbar_init(
+    &p_widget_listbox->scrollbar,
+    NULL,
+    width-10,
+    0,
+    10,
+    height,
+    SCROLLBAR_VERTICAL
   );
 
   /* No items. */
   p_widget_listbox->n_items = 0;
-  p_widget_listbox->selected_index = -1;
+  p_widget_listbox->p_selected_item = NULL;
+
+  /* Animation. */
+  p_widget_listbox->state = LB_STATE_IDLE;
+  p_widget_listbox->move_orig_x = 0;
+  p_widget_listbox->move_orig_y = 0;
+  p_widget_listbox->offset = 0;
 
   /* Set user data. */
   widget_set_userdata(&p_widget_listbox->widget, (void *)p_widget_listbox);
 
   /* Set drawing function. */
   widget_set_drawfunc(&p_widget_listbox->widget, widget_listbox_drawfunc);
+
+  /* Set default event handler. */
+  widget_set_eventhandler(&p_widget_listbox->widget, widget_listbox_event_handler);
 }
+
+
+/**
+ * widget_listbox_add()
+ * 
+ * @brief: Add widget to listbox
+ * @param p_widget_listbox: Pointer to a `widget_listbox_t` structure
+ * @param p_widget: Pointer to a `widget_t` structure
+ **/
 
 void widget_listbox_add(widget_listbox_t *p_widget_listbox, widget_t *p_widget)
 {
@@ -106,9 +353,69 @@ void widget_listbox_add(widget_listbox_t *p_widget_listbox, widget_t *p_widget)
   /* Add widget to container. */
   widget_container_add(&p_widget_listbox->container, p_widget);
 
+  /* Update scrollbar. */
+  update_scrollbar(p_widget_listbox);
 }
+
+
+/**
+ * widget_listbox_remove()
+ * 
+ * @brief: Remove widget from listbox.
+ * @param p_widget_listbox: pointer to a `widget_listbox_t` structure.
+ * @param p_widget: pointer to a `widget_t`structure representing the widget to remove.
+ **/
 
 void widget_listbox_remove(widget_listbox_t *p_widget_listbox, widget_t *p_widget)
 {
+  widget_container_item_t *p_item = NULL;
 
+  /* Remove widget. */
+  widget_container_remove(&p_widget_listbox->container, p_widget);
+
+  /* Update widgets relative box. */
+  if (p_widget_listbox->container.p_children != NULL)
+  {
+    p_item = p_widget_listbox->container.p_children;
+    while (p_item != NULL)
+    {
+      /* Ensure first item starts at the correct position. */
+      if (p_item == p_widget_listbox->container.p_children)
+      {
+        p_item->rel_box.y = 2;
+      }
+      
+      /* Following item must be updated, if any. */
+      if (p_item->p_next != NULL)
+      {
+        p_item->p_next->rel_box.y = p_item->rel_box.y + p_item->rel_box.height;
+      }
+
+      /* Process next item. */
+      p_item = p_item->p_next;
+    }
+  }
+
+  /* Update scrollbar. */
+  update_scrollbar(p_widget_listbox);
+}
+
+
+int widget_listbox_count(widget_listbox_t *p_widget_listbox)
+{
+  int n=0;
+  widget_container_item_t *p_item = NULL;
+
+  if (p_widget_listbox->container.p_children != NULL)
+  {
+    n = 1;
+    p_item = p_widget_listbox->container.p_children;
+    while (p_item->p_next != NULL)
+    {
+      n++;
+      p_item = p_item->p_next;
+    }
+  }
+
+  return n;
 }
