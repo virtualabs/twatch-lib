a6771c6b9b5f34e7d3ef4d9fac8ccfac642818cb
virtualabs <virtualabs@gmail.com>
2021-03-25 23:04:48 +0100

    Added background color support and font size (normal/small).

--- inc/ui/label.h
+++ inc/ui/label.h
@@ -8,15 +8,24 @@
 
 #define LABEL_STYLE_TEXT RGB(0xf, 0xf, 0xf)
 
+typedef enum {
+  LABEL_FONT_NORMAL,
+  LABEL_FONT_SMALL
+} widget_label_fontsize_t;
+
 typedef struct {
   widget_t widget;
 
   /* Button label. */
   char *psz_label;
 
+  /* Font size. */
+  widget_label_fontsize_t font_size;
+
 } widget_label_t;
 
 void widget_label_init(widget_label_t *p_widget_label, tile_t *p_tile, int x, int y, int width, int height, char *psz_label);
 void widget_label_set_text(widget_label_t *p_widget_label, char *psz_label);
+void widget_label_set_fontsize(widget_label_t *p_widget_label, widget_label_fontsize_t font_size);
 
 #endif /* __INC_WIDGET_LABEL_H */
--- ui/label.c
+++ ui/label.c
@@ -13,14 +13,37 @@ void widget_label_drawfunc(widget_t *p_widget)
 
   if (p_label->psz_label != NULL)
     {
-      /* Draw text. */
-      widget_draw_text_x2(
+      /* Draw background. */
+      widget_fill_region(
         p_widget,
-        3,
-        3,
-        p_label->psz_label,
-        p_widget->style.front
+        1,
+        1,
+        p_widget->box.width-2,
+        p_widget->box.height-2,
+        p_widget->style.background
       );
+
+      /* Draw text. */
+      if (p_label->font_size == LABEL_FONT_NORMAL)
+      {
+        widget_draw_text_x2(
+          p_widget,
+          3,
+          3,
+          p_label->psz_label,
+          p_widget->style.front
+        );
+      }
+      else
+      {
+        widget_draw_text(
+          p_widget,
+          3,
+          3,
+          p_label->psz_label,
+          p_widget->style.front
+        );
+      }
     }
 }
 
@@ -43,6 +66,7 @@ void widget_label_init(widget_label_t *p_widget_label, tile_t *p_tile, int x, in
 
   /* Set properties. */
   p_widget_label->psz_label = psz_label;
+  p_widget_label->font_size = LABEL_FONT_NORMAL;
 
   /* Set user data. */
   widget_set_userdata(&p_widget_label->widget, (void *)p_widget_label);
@@ -61,4 +85,16 @@ void widget_label_init(widget_label_t *p_widget_label, tile_t *p_tile, int x, in
 void widget_label_set_text(widget_label_t *p_widget_label, char *psz_label)
 {
   p_widget_label->psz_label = psz_label;
+}
+
+
+/**
+ * @brief: Set label font size
+ * @param p_widget_label: pointer to a `widget_label_t` structure
+ * @param font_size: font size (either LABEL_FONT_NORMAL or LABEL_FONT_SMALL)
+ **/
+
+void widget_label_set_fontsize(widget_label_t *p_widget_label, widget_label_fontsize_t font_size)
+{
+  p_widget_label->font_size = font_size;
 }
