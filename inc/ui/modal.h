4ddd246315879ce82ee539bc97cfcd84755114e0
virtualabs <virtualabs@gmail.com>
2021-07-01 22:56:18 +0200

    Made modal border color customizable.

--- inc/ui/modal.h
+++ inc/ui/modal.h
@@ -6,8 +6,12 @@ typedef struct {
   /* Underlying tile. */
   tile_t tile;
 
+  /* Border color. */
+  uint16_t border_color;
+
 } modal_t;
 
 void modal_init(modal_t *p_modal, int x, int y, int width, int height);
+void modal_set_border_color(modal_t *p_modal, uint16_t color);
 
 #endif /* __INC_UI_MODAL_H */
--- ui/modal.c
+++ ui/modal.c
@@ -38,7 +38,9 @@ int _modal_event_handler(tile_t *p_tile, tile_event_t p_event, int x, int y, int
 
 int _modal_drawfunc(tile_t *p_tile)
 {
-  if (p_tile != NULL)
+  modal_t *p_modal = p_tile->p_user_data;
+
+  if (p_modal != NULL)
   {
 
     /* Set drawing window. */
@@ -53,43 +55,51 @@ int _modal_drawfunc(tile_t *p_tile)
       p_tile,
       1,
       1,
-      p_tile->width-2,
-      p_tile->height-2,
+      p_tile->offset_x + p_tile->width-2,
+      p_tile->offset_y + p_tile->height-2,
       p_tile->background_color
     );
 
     /* Draw the modal. */
+
+    /* Draw top line. */
     tile_draw_line(
       p_tile,
       1,
       0,
-      p_tile->width - 2,
+      p_tile->offset_x + p_tile->width - 2,
       0,
-      RGB(0xf,0xf,0xf)
+      p_modal->border_color
     );
+
+    /* Draw bottom line. */
     tile_draw_line(
       p_tile,
       1,
-      p_tile->height - 1,
-      p_tile->width - 2,
-      p_tile->height - 1,
-      RGB(0xf,0xf,0xf)
+      p_tile->offset_y + p_tile->height - 1,
+      p_tile->offset_x + p_tile->width - 2,
+      p_tile->offset_y + p_tile->height - 1,
+      p_modal->border_color
     );
+
+    /* Draw left line. */
     tile_draw_line(
       p_tile,
       0,
       1,
       0,
-      p_tile->height  - 2,
-      RGB(0xf,0xf,0xf)
+      p_tile->offset_y + p_tile->height  - 2,
+      p_modal->border_color
     );
+
+    /* Draw right line. */
     tile_draw_line(
       p_tile,
-      p_tile->width - 1,
+      p_tile->offset_x + p_tile->width - 1,
       1,
-      p_tile->width - 1,
-      p_tile->height  - 2,
-      RGB(0xf,0xf,0xf)
+      p_tile->offset_x + p_tile->width - 1,
+      p_tile->offset_y + p_tile->height  - 2,
+      p_modal->border_color
     );
   }
 
@@ -120,7 +130,25 @@ void modal_init(modal_t *p_modal, int x, int y, int width, int height)
   p_modal->tile.width = width;
   p_modal->tile.height = height;
 
+  /* Set default border color (white). */
+  p_modal->border_color = RGB(0xf, 0xf, 0xf);
+
   /* Set drawing function. */
   tile_set_drawfunc(&p_modal->tile, _modal_drawfunc);
   tile_set_event_handler(&p_modal->tile, _modal_event_handler);
+}
+
+
+/**
+ * modal_set_border_color()
+ * 
+ * @brief: initialize a modal dialog box.
+ * @param p_modal: pointer to a `modal_t` structure
+ * @param color: border color to use with this modal
+ **/
+
+void modal_set_border_color(modal_t *p_modal, uint16_t color)
+{
+  /* Set border color. */
+  p_modal->border_color = color;
 }
